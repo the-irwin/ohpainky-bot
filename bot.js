bcd5a2c3609e15da776d0fa193fdab6439b0479d
the-irwin <lbt12to12@gmail.com>
2020-08-08 13:39:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,6 +46,16 @@ var emojiname = ["ohiored", "ohiopurple"],
 //});
 
 client.on("messageReactionAdd", (reaction, user) => {
+    if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
     if (user && !user.bot && reaction.message.channel == channel)
     //if (user && !user.bot && reaction.message.channel.guild)
         console.log(reaction.emoji.name);
@@ -57,6 +67,16 @@ client.on("messageReactionAdd", (reaction, user) => {
 });
 
 client.on("messageReactionRemove", (reaction, user) => {
+    if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
     if (user && !user.bot && reaction.message.channel == channel)
         for (let o in emojiname)
             if (reaction.emoji.name == emojiname[o]) {
