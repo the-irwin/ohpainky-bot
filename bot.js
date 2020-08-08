974ef13abc81fd6360c5586bbba31bfa5b630ea9
the-irwin <lbt12to12@gmail.com>
2020-08-08 11:45:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,6 @@ client.on('ready', () => {
         
     
         sendMessage(); // send the message once
-        var minMillseconds = 1000 * 60 * 6 * 60;
-        setInterval(function(){ // repeat this every 24 hours
-            sendMessage();
-        }, minMillseconds)
 });
 
 client.on('message', message => {
@@ -25,11 +21,43 @@ client.on('message', message => {
 });
 
 function sendMessage(){
-    var guild = client.guilds.get('727285430499672115');
-    var channel = guild.channels.get('727285430499672119');
+    var guild = client.guilds.get('692591742570201118');
+    var channel = guild.channels.get('741678960383099000');
     channel.send("Hello");
     
 }
+
+var emojiname = ["ohiored", "ohiopurple"],
+    rolename = ["new role", ""];
+
+client.on("message", e => {
+    if (e.content.startsWith(prefix + "reaction")) {
+        if (!e.channel.guild) return;
+        for (let o in emojiname) {
+            var n = [e.guild.emojis.find(e => e.name == emojiname[o])];
+            for (let o in n) e.react(n[o])
+        }
+    }
+});
+
+client.on("messageReactionAdd", (e, n) => {
+    if (n && !n.bot && e.message.channel.guild)
+        for (let o in emojiname)
+            if (e.emoji.name == emojiname[o]) {
+                let i = e.message.guild.roles.find(e => e.name == rolename[o]);
+                e.message.guild.member(n).addRole(i).catch(console.error)
+            }
+});
+
+client.on("messageReactionRemove", (e, n) => {
+    if (n && !n.bot && e.message.channel.guild)
+        for (let o in emojiname)
+            if (e.emoji.name == emojiname[o]) {
+                let i = e.message.guild.roles.find(e => e.name == rolename[o]);
+                e.message.guild.member(n).removeRole(i).catch(console.error)
+            }
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
