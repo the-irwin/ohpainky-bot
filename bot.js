5f99d2d424c709b6f45e2665dd1c937aaacea4df
the-irwin <lbt12to12@gmail.com>
2020-08-16 21:45:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('message', message => {
     }
     if(message.content.toLowerCase().includes('irwin') {
         console.log("notifying Irwin");
-        Message.guild.members.cache.get('520732521277685765').send("You've been mentioned!\nhttp://discordapp.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
+        message.guild.members.cache.get('520732521277685765').send("You've been mentioned!\nhttp://discordapp.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
     }
 });
 
