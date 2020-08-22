1585bc1871e00a18e58d128488fa5314c037fc02
the-irwin <lbt12to12@gmail.com>
2020-08-22 17:07:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,12 +54,13 @@ client.on('message', message => {
     if (messageString === 'ping') {
     	message.reply('pong');
   	}
+    messageString = messageString.replace(/<.*?S/>, '')
     if (messageString.toLowerCase().includes('eat you')) {
         message.reply('kinky');
     }
-    //if (messageString.includes('69')) {
-    //    message.reply('nice');
-    //}
+    if (messageString.includes('69')) {
+        message.reply('nice');
+    }
     if(messageString.toLowerCase() === 'michigan' || messageString.toLowerCase() === 'michigay') {
         message.reply('Boo Michigan!');
     }
