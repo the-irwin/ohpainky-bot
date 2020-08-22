d36b04373f08943205c354742c84f55712bd55e3
the-irwin <lbt12to12@gmail.com>
2020-08-21 22:05:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,9 +30,9 @@ client.on('ready', () => {
     
     //sendMessage(); // send the message once
     
-    const botCommandsChannel = client.guilds.get('727285430499672115').channels.get(botCommandsChannelId);;
+    const botCommandsChannel = client.guilds.get('727285430499672115').channels.get(botCommandsChannelId);
     
-    botCommandsChannel.messages.fetch({ limit: 100 }).then(messages => {
+    botCommandsChannel.messages.fetch().then(messages => {
     console.log(`Received ${messages.size} messages`);
     //Iterate through the messages here with the variable "messages".
     messages.forEach(message => importBotCommand(message.content));
