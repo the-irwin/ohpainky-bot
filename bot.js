28a4bf36bd4654c3042bccdebe9c932b60dce110
the-irwin <lbt12to12@gmail.com>
2020-11-12 13:29:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 'use strict';
 const Discord = require('discord.js');
-const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD'] });
 const sRoleChannelId = '741760525045727243';
 const roleChannelId = '707375163171143701';
 const botChannelId = '741678960383099000';
