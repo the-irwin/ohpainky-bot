fbc0432431aed5655cc3a8a54105f1adfb5ea5e2
the-irwin <lbt12to12@gmail.com>
2020-11-20 17:55:50 -0500

    Added link to #general
    added link to #general in the join notification message

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const sRoleChannelId = '741760525045727243';
 const roleChannelId = '707375163171143701';
 const botChannelId = '741678960383099000';
 const botCommandsChannelId = '746580589020315738';
+const generalChannelId = '692591742570201124';
 const showcaseChannelId = '696773761239875615';
 const joinNotifChannelId = '696226678644408362';
 const pongRoleId = '771362128522772520';
@@ -85,7 +86,7 @@ client.on('messageDelete', message => {
 
 client.on('guildMemberAdd', member => {
     client.channels.fetch(joinNotifChannelId).then(joinNotifChannel => {
-        joinNotifChannel.send(`Yo, ${pongRole}, ${member} just joined!`);
+        joinNotifChannel.send(`Yo, ${pongRole}, ${member} just joined! Go to ${client.channels.fetch(generalChannel)} and welcome them!`);
     });
 });
 
